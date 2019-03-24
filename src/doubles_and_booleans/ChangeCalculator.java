003abc371b950bf7e0d02f031e25bf88e4df1762
CesarTadeo <league@imac-6.attlocal.net>
2019-03-23 17:32:09 -0700

    I did Change Chalculater today

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,28 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickles = JOptionPane.showInputDialog(null, "how many nickles do you have?");
+		
 		// Convert their answer to an int using Integer.parseInt()
-
+		int Nickles = Integer.parseInt(nickles);
+		int TotalOfNickles = Nickles * 5;
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimes = JOptionPane.showInputDialog(null, "how many dimes do you have?");
+	
 		// Ask the user how many quarters they have, and convert their answer
-
+		int Dimes = Integer.parseInt(dimes);
+		int TotalOfDimes = Dimes * 10;
+		
+		
+		
+		String quarters = JOptionPane.showInputDialog(null, "how many quarters do you have");
+		
+		int Quarters = Integer.parseInt(quarters);
+		int TotlaOfQuarters = Quarters * 25; 
 		// Calculate how much money the user has and save it in a double variable 
-
+		int total = TotalOfDimes + TotalOfNickles + TotalOfDimes; 
 		// Tell the user how much money they have
-
+		JOptionPane.showMessageDialog(null, total);
 	}
 }
 
