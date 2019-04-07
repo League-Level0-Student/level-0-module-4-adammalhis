aab74954061b5d3a445d5cb7e9e2767faa203149
league <league@wts14.attlocal.net>
2019-04-06 17:25:45 -0700

    I DID SLEE[Y HEAD AND STRING METHODS TODAY!

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -1,32 +1,36 @@
 package charAt_scanners_optionDialogs;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
-import javax.swing.JOptionPane;
+//    Level 0
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
-		// 1. Create a String variable and initialize it to whatever you want
-		String variable = "hello my name is Adam";   
-		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		System.out.println(variable.charAt(2));
-		
-		// 3. Print the length of your String to the console.
-		//    HINT: .length()
-		System.out.println();
-		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
 
-		
+		// 1. Create a String variable and initialize it to whatever you want
+		String variable = "hello my name is Adam";
+		// 2. Print the 3rd char of your String to the console.
+		// HINT: .charAt
+		System.out.println(variable.charAt(2));
+
+		// 3. Print the length of your String to the console.
+		// HINT: .length()
+		System.out.println(variable.length());
+		// 4. Using a for loop, print one char at time to the console.
+		// HINT: use .length() to determine how many loops
+		for (int i = 0; i < variable.length(); i++) {
+			System.out.println(variable.charAt(i));
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
-		//    what position/index in the String the char is located.
-		//    Print the char's position to the console.
-		//    EXAMPLE: if your string is "abc" and you are searching 
-		//             for char 'b', then print "b is at index 1"
-		
+		// what position/index in the String the char is located.
+		// Print the char's position to the console.
+		// EXAMPLE: if your string is "abc" and you are searching
+		// for char 'b', then print "b is at index 1"
+		for (int i = 0; i < variable.length(); i++) {
+			if (variable.charAt(i) == 'A') {
+				System.out.println(i);
+			}
+
+		}
+
 	}
 }
-
-
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,63 +1,57 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-       
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-       
-   
-    
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-      */
-       int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-        if (weekday == 0) {
-        	isWeekday = true;
-        	
-        	
-        }
-        else {
-        	isWeekday = false; 
-        }
-        
-        
-        
-        int vacation = JOptionPane.showConfirmDialog(null,  "is it a vacation?", "sleepyHead", JOptionPane. YES_NO_OPTION); 
-        if (vacation == 0) {
-        	isVacation = true;
-        }
-        
-        else {
-        	isVacation = false;
-        }
-        if (isVacation == true || isWeekday == false) {
-        	System.out.println("sleep in");
-        	
-        	
-        	}
-        
-        
-        
-        else if (isWeekday == true) {
-        	System.out.println("get up lazybones!");
-        }
-        
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         *
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
-         */
-    }
+		boolean isWeekday, isVacation;
+
+		/* You MUST use the above boolean variables in your code */
+
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 * 
+		 * 
+		 * 
+		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+		int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		if (weekday == 0) {
+			isWeekday = true;
+
+		} else {
+			isWeekday = false;
+		}
+
+		int vacation = JOptionPane.showConfirmDialog(null, "is it a vacation?", "sleepyHead",
+				JOptionPane.YES_NO_OPTION);
+		if (vacation == 0) {
+			isVacation = true;
+		}
+
+		else {
+			isVacation = false;
+		}
+		if (isVacation == true || isWeekday == false) {
+			System.out.println("sleep in");
+
+		}
+
+		else if (isWeekday == true) {
+			System.out.println("get up lazybones!");
+		}
+
+		/*
+		 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
+		 *
+		 * print “get up lazybones!�? If it is a weekday, and we are on vacation, print
+		 * “sleep in�?.
+		 */
+	}
 }
